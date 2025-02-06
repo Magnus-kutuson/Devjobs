ddc6eab6f586a669ccaaa6a93432f0841abc26a9
Magnus <bcict20070@ttu.edu.gh>
2025-02-06 16:37:14 +0000

    feat: update header component styles and structure for theme switcher

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { InputsComponent } from '../inputs/inputs.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [InputsComponent, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
