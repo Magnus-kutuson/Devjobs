17b08e36f9d7e5b8afbae4382047820bbfdc1cbb
Magnus <bcict20070@ttu.edu.gh>
2025-02-07 10:58:42 +0000

    feat: replace header component with layout component in app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/components/header/header.component';
+import { LayoutComponent } from './core/pages/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
