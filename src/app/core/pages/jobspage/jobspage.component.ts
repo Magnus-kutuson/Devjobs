341dff863a1212c16487a08bcb3bca9290b7d321
Magnus <bcict20070@ttu.edu.gh>
2025-02-07 13:46:47 +0000

    feat: enhance jobspage with data fetching and display functionality

--- src/app/core/pages/jobspage/jobspage.component.ts
+++ src/app/core/pages/jobspage/jobspage.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data/data.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-jobspage',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './jobspage.component.html',
   styleUrl: './jobspage.component.css'
 })
 export class JobspageComponent {
+  jobs: string[] = [];
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit() {
+    this.dataService.getJobs().subscribe(data => {
+      console.log(this.jobs = data.jobs);
+    });
+  }
 
 }
+
